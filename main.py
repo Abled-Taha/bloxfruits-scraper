beb63b79ff1e028aa914c525b3820e6f87340b9e
Abled-Taha <abledtaha@gmail.com>
2025-09-10 23:26:48 +0500

    Added Docker Integration.

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import os
-import dotenv
+import dotenv, subprocess
 
 dotenv.load_dotenv()
 debug = os.getenv("DEBUG", "False").lower() in ("true", "1", "t")
 
 if __name__ == "__main__":
-  from src.flask import app
-  app.run(debug=debug)
+    if debug:
+        from src.flask import app
+        app.run(host="0.0.0.0", port=5000, debug=True)
+    else:
+        subprocess.call(["waitress-serve", f"--listen=0.0.0.0:5000", "src.flask:app"])
   
